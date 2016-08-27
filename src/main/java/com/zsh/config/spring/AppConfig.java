ee4fc35d1c996aca9698dfce843fdce80ca9ed0d
yuantanglin081220 <857882241@qq.com>
2016-08-27 16:03:12 +0800

    portal
    Spring mvc framework

---
+++ src/main/java/com/zsh/config/spring/AppConfig.java
@@ -0,0 +1,10 @@
+package com.zsh.config.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.zsh.*")
+public class AppConfig {
+
+}
